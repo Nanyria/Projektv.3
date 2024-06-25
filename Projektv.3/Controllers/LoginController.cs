afa09b5d351a1b4e8935388a599ce74a56a5a718
Nanyria <145432308+Nanyria@users.noreply.github.com>
2024-06-25 13:35:21 +0200

    Add project files.

---
+++ Projektv.3/Controllers/LoginController.cs
@@ -0,0 +1,143 @@
+﻿
+using IndProjModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Indiv.Uppgiftv2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login(LoginDTO login)
+        {
+            if (login.Username == "Admin1001" && login.Password == "123Password")
+            {
+                var token = GenerateJwtToken();
+                return Ok(new { token });
+            }
+            return Unauthorized();
+        }
+
+        private string GenerateJwtToken()
+        {
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes("YourSecretKeyForAuthenticationOfApplication");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", "1") }),
+                Expires = DateTime.UtcNow.AddHours(3),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = "youtCompanyIssuer.com",
+                Audience = "youtCompanyIssuer.com"
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
+    //{
+    //    [HttpPost("login")]
+    //    public IActionResult Login(LoginDTO login)
+    //    {
+    //        if (login.Username == "Admin1001" && login.Password == "123Password")
+    //        {
+    //            var token = GenerateJwtToken();
+    //            return Ok(new { token });
+    //        }
+    //        return Unauthorized();
+    //    }
+
+    //    private string GenerateJwtToken()
+    //    {
+    //        var tokenHandler = new JwtSecurityTokenHandler();
+    //        var key = Encoding.UTF8.GetBytes("G7DkUJneKl1Z3YRpQF6sjV8hT3mC9gX5");
+    //        var tokenDescriptor = new SecurityTokenDescriptor
+    //        {
+    //            Subject = new ClaimsIdentity(new[] { new Claim("id", "1") }),
+    //            Expires = DateTime.UtcNow.AddHours(1),
+    //            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+    //            Issuer = "http://localhost",
+    //            Audience = "http://localhost"
+    //        };
+    //        var token = tokenHandler.CreateToken(tokenDescriptor);
+    //        return tokenHandler.WriteToken(token);
+    //    }
+    //}
+    //public IActionResult Login(LoginDTO loginDTO)
+    //{
+    //    try
+    //    {
+    //        if (string.IsNullOrEmpty(loginDTO.UserID) ||
+    //                string.IsNullOrEmpty(loginDTO.Password))
+    //            return BadRequest("Username and/or password not specified.");
+    //        if (loginDTO.UserID.Equals("Admin1001") && loginDTO.Password.Equals("123Pasword"))
+    //        {
+    //            var secretKey = new SymmetricSecurityKey
+    //                (Encoding.UTF8.GetBytes("thisisasecretkey@123"));
+
+    //            var signinCredentials = new SigningCredentials
+    //                (secretKey, SecurityAlgorithms.HmacSha256);
+
+    //            var jwtSecurityToken = new JwtSecurityToken(
+    //                issuer: "NaWi",
+    //                audience: "https://localhost:7203",
+    //                claims: new List<Claim>(),
+    //                expires: DateTime.Now.AddMinutes(10),
+    //                signingCredentials: signinCredentials
+    //                );
+    //            Ok(new JwtSecurityTokenHandler()
+    //                .WriteToken(jwtSecurityToken));
+    //        }
+    //    }
+    //    catch (Exception)
+    //    {
+    //        return BadRequest
+    //        ("An error occurred in generating the token");
+    //    }
+    //    return Unauthorized();
+    //}
+
+
+    //[HttpPost, Route("login")]
+    //public IActionResult Login(LoginDTO loginDTO)
+    //{
+    //    try
+    //    {
+    //        if (string.IsNullOrEmpty(loginDTO.UserID) ||
+    //            string.IsNullOrEmpty(loginDTO.Password))
+    //        {
+    //            return BadRequest("Username and/or password not specified.");
+    //        }
+
+    //        if (loginDTO.UserID.Equals("Admin1001") && loginDTO.Password.Equals("123Password"))
+    //        {
+    //            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("thisisasecretkey@123"));
+    //            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+    //            var jwtSecurityToken = new JwtSecurityToken(
+    //                issuer: "NaWi",
+    //                audience: "http://localhost:7203",
+    //                claims: new List<Claim>(),
+    //                expires: DateTime.Now.AddMinutes(10),
+    //                signingCredentials: signinCredentials
+    //            );
+
+    //            var tokenString = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+    //            return Ok(new { Token = tokenString });
+    //        }
+
+    //        return Unauthorized();
+    //    }
+    //    catch (Exception)
+    //    {
+    //        return BadRequest("An error occurred in generating the token");
+    //    }
+    //}
+
+
---
+++ Projektv.3/Program.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Jwt configuration starts here
+var jwtIssuer = builder.Configuration.GetSection("Jwt:Issuer").Get<string>();
+var jwtKey = builder.Configuration.GetSection("Jwt:Key").Get<string>();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    // Add JWT authentication to Swagger
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter into field the word 'Bearer' followed by a space and the JWT token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+ .AddJwtBearer(options =>
+ {
+     options.TokenValidationParameters = new TokenValidationParameters
+     {
+         ValidateIssuer = true,
+         ValidateAudience = true,
+         ValidateLifetime = true,
+         ValidateIssuerSigningKey = true,
+         ValidIssuer = jwtIssuer,
+         ValidAudience = jwtIssuer,
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+     };
+ });
+//Jwt configuration ends here
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
